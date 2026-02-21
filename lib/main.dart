23a23f507cdb1d0a806e0002ce985543b85c40af
Aman Sikarwar <amansik.1910@gmail.com>
2026-02-21 23:43:33 +0530

    chore: update dependencies in pubspec.yaml for improved functionality and performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:honk/config/env.dart';
 import 'package:honk/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseAnonKey);
   runApp(const MainApp());
 }
 
